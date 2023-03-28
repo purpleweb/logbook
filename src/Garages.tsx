0b5365ece69100c40e55e74726c786af96cd1541
Purpleweb <tonio.cz@gmail.com>
2023-03-28 21:07:56 +0200

    remove useless html in components

--- src/Garages.tsx
+++ src/Garages.tsx
@@ -3,59 +3,6 @@ export function Garages() {
   const title = 'All garages'
 
   return (
-    <main className="content">
-      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
-        <div className="d-block mb-4 mb-md-0">
-          <nav aria-label="breadcrumb" className="d-none d-md-inline-block">
-            <ol className="breadcrumb breadcrumb-dark breadcrumb-transparent">
-              <li className="breadcrumb-item">
-                <a href="#">
-                  <svg
-                    className="icon icon-xxs"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    ></path>
-                  </svg>
-                </a>
-              </li>
-              <li className="breadcrumb-item active" aria-current="page">
-                {breadcrumbTitle}
-              </li>
-            </ol>
-          </nav>
-          <h2 className="h4">{title}</h2>
-        </div>
-        <div className="btn-toolbar mb-2 mb-md-0">
-          <a
-            href="#"
-            className="btn btn-sm btn-gray-800 d-inline-flex align-items-center"
-          >
-            <svg
-              className="icon icon-xs me-2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              ></path>
-            </svg>{" "}
-            New log
-          </a>
-        </div>
-      </div>
-    </main>
-  );
+    <h1>{title}</h1>
+  )
 }
