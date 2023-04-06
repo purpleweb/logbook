64332807ee7273638d89d0d018f3d2af5fb2417d
Purpleweb <tonio.cz@gmail.com>
2023-04-06 15:06:01 +0200

    api test_get_interventions and test_delete_intervention

--- api/app/tests/test_api.py
+++ api/app/tests/test_api.py
@@ -32,6 +32,76 @@ def test_get_garages():
         }
     ]
 
+def test_get_interventions():
+    response = client.get('/interventions/')
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": 2,
+            "date": "2021-01-10",
+            "km": 110000,
+            "cost": 500,
+            "description": None,
+            "garage": {
+                "name": "Speedy Versailles Chantier",
+                "phone": "01 39 49 06 88",
+                "id": 2
+            },
+            "operations": [
+                {
+                    "id": 1,
+                    "title": "Révision"
+                }
+            ]
+        },
+        {
+            "id": 1,
+            "date": "2022-11-16",
+            "km": 122000,
+            "cost": 320.5,
+            "description": "passage au garage",
+            "garage": {
+                "name": "Speedy le Chesnay",
+                "phone": "01 39 56 56 45",
+                "id": 1
+            },
+            "operations": [
+                {
+                    "id": 4,
+                    "title": "Vidange"
+                },
+                {
+                    "id": 5,
+                    "title": "Plaquettes AV"
+                }
+            ]
+        }
+    ]
+
+
 def test_delete_intervention():
     response = client.delete('/interventions/1')
     assert response.status_code == 200
+
+    response = client.get('/interventions/')
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": 2,
+            "date": "2021-01-10",
+            "km": 110000,
+            "cost": 500,
+            "description": None,
+            "garage": {
+                "name": "Speedy Versailles Chantier",
+                "phone": "01 39 49 06 88",
+                "id": 2
+            },
+            "operations": [
+                {
+                    "id": 1,
+                    "title": "Révision"
+                }
+            ]
+        },
+    ]
