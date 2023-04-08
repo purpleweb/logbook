3e78381890b528ef36153f2356e81d011a950e69
Purpleweb <tonio.cz@gmail.com>
2023-04-08 14:16:13 +0200

    use reset data fixture in api tests

--- api/app/tests/test_api.py
+++ api/app/tests/test_api.py
@@ -1,10 +1,33 @@
+from sqlalchemy import create_engine, StaticPool
+from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
+from ..models import Base
 from ..main import app, get_db
-from .db import reset_db, override_get_db
+from ..data import create_data
+from pytest import fixture
 
-reset_db()
+engine = create_engine('sqlite:///:memory:', echo=True, connect_args={"check_same_thread": False}, poolclass=StaticPool)
+TestingSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(engine)
+session = TestingSession()
+
+def override_get_db():
+    try:
+        db = TestingSession()
+        yield db
+    finally:
+        db.close()
+
 app.dependency_overrides[get_db] = override_get_db
+
 client = TestClient(app)
+
+@fixture(autouse=True)
+def reset():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    create_data(session=session)
+
 
 def test_get_garages():
     response = client.get('/garages/')
@@ -31,6 +54,19 @@ def test_get_garages():
             "id": 4
         }
     ]
+
+
+def test_delete_intervention():
+    response = client.delete('/interventions/1')
+    assert response.status_code == 200
+
+    response = client.get('/interventions/')
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) == 1
+    assert isinstance(response.json()[0], dict)
+    assert response.json()[0].get('id') == 2
+
 
 def test_get_interventions():
     response = client.get('/interventions/')
@@ -76,32 +112,4 @@ def test_get_interventions():
                 }
             ]
         }
-    ]
-
-
-def test_delete_intervention():
-    response = client.delete('/interventions/1')
-    assert response.status_code == 200
-
-    response = client.get('/interventions/')
-    assert response.status_code == 200
-    assert response.json() == [
-        {
-            "id": 2,
-            "date": "2021-01-10",
-            "km": 110000,
-            "cost": 500,
-            "description": None,
-            "garage": {
-                "name": "Speedy Versailles Chantier",
-                "phone": "01 39 49 06 88",
-                "id": 2
-            },
-            "operations": [
-                {
-                    "id": 1,
-                    "title": "Révision"
-                }
-            ]
-        },
     ]
