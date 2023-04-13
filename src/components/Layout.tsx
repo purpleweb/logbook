43a3ade5094a09c1f6c885154840cdd8682d7eb0
Purpleweb <tonio.cz@gmail.com>
2023-04-13 11:20:19 +0200

    web: remove unused import in Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
 
